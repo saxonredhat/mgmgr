2d0aee7cf4c711309a547952411ad4dfb4066341
saxon.redhat <saxon.redhat@gmail.com>
2022-05-12 00:52:53 +0800

    v1.0.0 fixbug

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, Geektutu")
+		c.String(200, "Hello, Geektutu,fixbugs")
 	})
 	r.Run(":8000") // listen and serve on 0.0.0.0:8000
 }
